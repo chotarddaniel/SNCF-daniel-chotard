d8ce195d2eb9f3f0ced8e5826dc4bce24317a618
chotarddaniel <31211323+chotarddaniel@users.noreply.github.com>
2018-10-17 23:17:03 +0200

    Création du select 1 seul valeur retourner

--- html/index.php
+++ html/index.php
@@ -5,25 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>SNCF</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="main.js"></script>
+    
 </head>
 <body>
     <?php
         include '../PHP/cnx.php';
         $sql = $bdd->prepare("select numero, libelle from activite");
         $sql->execute();
-
-        foreach($sql->fetchALL(PDO::FETCH_ASSOC) as $ligne)
-        {
-            echo "
-            <select>
-                <option></option>
-            </select>";
-            echo $ligne['numero']." ".$ligne['libelle']."<br>";
-            
-        }
-        
+   
     ?>
+
+<select name=test_affichage_d_une_valeur>
+    <?php foreach($sql->fetchALL(PDO::FETCH_ASSOC) as $ligne){}?>
+    <option>
+        <?php echo $ligne["libelle"]?>
+    </option>
     
+</select>
+
 </body>
 </html>
