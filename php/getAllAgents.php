e077d7280737fc27bf43fb2dad0cda87b5b82c29
chotarddaniel <31211323+chotarddaniel@users.noreply.github.com>
2018-11-05 13:29:11 +0100

    Affichage des agents dans un tableaux

--- php/getAllAgents.php
+++ php/getAllAgents.php
@@ -2,16 +2,27 @@
 
 include "cnx.php";
 
-$sql2 = $bdd->prepare("select prenom from agent where numeroActivite = ".$_GET['num2']);
+$sql2 = $bdd->prepare("select numeroFormation, codeAgent, presence from inscription where numeroFormation ='".$_GET['num2']."'");
 $sql2-> execute();
 
 foreach($sql2->fetchAll(PDO::FETCH_ASSOC) as $ligne)
 {
-    echo "<p>".$ligne['prenom']."</p>";
-   /* echo "<p>".$ligne['nom']."</p>";
-    echo "<p>".$ligne['adresse']."</p>";
-    echo "<p>".$ligne['codePostal']."</p>";
-    echo "<p>".$ligne['ville']."</p>";*/
+    $sql = $bdd->prepare("select * from agent where code ='".$ligne['codeAgent']."'");
+    $sql->execute();
+
+    foreach($sql->fetchAll(PDO::FETCH_ASSOC) as $ligne1)
+    {
+        echo "<table>";
+        echo "<tr>";
+        echo "<td>presence:".$ligne['presence']."</td>";
+        echo "<td>".$ligne1['civilite']."</td>";
+        echo "<td>".$ligne1['prenom']."</td>";
+        echo "<td>".$ligne1['nom']."</td>";
+        
+        echo "</tr>";
+
+        echo "</table>";
+    }
 }
 
 ?>
