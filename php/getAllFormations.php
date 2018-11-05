9fa839d45462a2563d5995b75fe8e7ad1ae011bb
chotarddaniel <31211323+chotarddaniel@users.noreply.github.com>
2018-11-05 13:14:16 +0100

    Affichage des nom avec les radio

--- php/getAllFormations.php
+++ php/getAllFormations.php
@@ -2,13 +2,13 @@
 
 include "cnx.php";
 
-$sql = $bdd->prepare("select intitule from formation where numeroActivite = ".$_GET['num']);
+$sql = $bdd->prepare("select intitule, code from formation where numeroActivite = ".$_GET['num']);
 $sql-> execute();
 
 foreach($sql->fetchAll(PDO::FETCH_ASSOC) as $ligne)
 {
-    echo "<p>".$ligne['intitule']."</p>";
+    echo "<p><input type='radio' name='test'value='".$ligne['code']."' onclick=RadioLesFormations() > ".$ligne['intitule']."</p>";
 }
 
-
+echo "<div id='divRdFormations'></div>";
 ?>
